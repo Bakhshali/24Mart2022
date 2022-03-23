9297f0a0bb177ef9ad1a0875e781ea8625c97e76
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-23 23:50:10 +0400

    Task 1 added

---
+++ Extention Task/Extention Task/Extention.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extention_Task
+{
+    static class Extention
+    {
+        public static bool Example(this string word, string words)
+        {
+            int i = 0;
+            if (char.IsUpper(word[i]))
+
+                foreach (char letter in word)                                          
+                {
+                    if (letter == word.Length)
+                    {
+                        i++;
+                    }
+                    else if (!char.IsLower(word[i]))
+                    {
+                        return true;
+                    }
+                    return false;
+                }                         
+            return false;
+        }
+    }
+}
---
+++ Extention Task/Extention Task/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Extention_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Bu bir custom contains methodudur";
+
+            Console.WriteLine(sentence.Example("Bu"));
+        }
+    }
+}
